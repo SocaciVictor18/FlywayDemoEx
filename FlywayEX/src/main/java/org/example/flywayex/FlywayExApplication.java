b59406576867ca253a3240351b54acb4192e18d4
Victor Socaci <Victor.Socaci@endava.com>
2026-01-05 15:53:10 +0200

    add

---
+++ FlywayEX/src/main/java/org/example/flywayex/FlywayExApplication.java
@@ -0,0 +1,13 @@
+package org.example.flywayex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlywayExApplication.class, args);
+    }
+
+}
---
+++ FlywayEX/src/test/java/org/example/flywayex/FlywayExApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.flywayex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
